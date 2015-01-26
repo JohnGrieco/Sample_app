6df228f97e132697231a8a9039603f7bfa369818
John Grieco <Johnjgrieco@gmail.com>
2015-01-26 15:55:38 -0800

    Test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
   test "should get home" do
-    get :home
+    get :about
     assert_response :success
   end
 
   test "should get help" do
-    get :help
     assert_response :success
   end
 
